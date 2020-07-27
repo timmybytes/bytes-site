1366b1b2835ee316bf2139c48be79b8c6c728cbd
Timothy Merritt <timmybytes@gmail.com>
2020-07-26 19:50:07 -0700

    Revised draft page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$("p").css("display", "none");
+$(".blog__card-link").on("click", function () {
+  $("p#test").slideToggle("fast");
+  // $("#1").slideToggle("fast");
+  // $("#2").slideToggle("fast");
+  // $("#3").slideToggle("fast");
+  // $("#4").slideToggle("fast");
+  // $("#5").slideToggle("fast");
+  // $("#6").slideToggle("fast");
+});
