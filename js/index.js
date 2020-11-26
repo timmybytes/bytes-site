3701220ffd4be9648474edfdbc211933f28d8bad
Timothy Merritt <timmybytes@gmail.com>
2020-11-26 06:21:08 -0800

    (refactor): Clean up JS localStorage functions

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const checkbox = document.getElementById('checkbox');
 
 // Save preference locally
+/** global: localStorage */
 if (localStorage.getItem('theme', 'dark')) {
   document.body.classList.add('dark');
   checkbox.checked = true;
@@ -13,7 +14,7 @@ if (themeSwitch) {
     resetTheme(); // update color theme
   });
 
-  function resetTheme() {
+  var resetTheme = () => {
     if (themeSwitch.checked) {
       // dark theme has been selected
       document.body.classList.add('dark');
@@ -22,7 +23,7 @@ if (themeSwitch) {
       document.body.classList.remove('dark');
       localStorage.removeItem('theme', 'dark'); // reset theme selection
     }
-  }
+  };
 }
 
 // Footer Copyright
