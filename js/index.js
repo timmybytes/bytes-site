80be23e865fa4682833b2cf0a594b7075701b76c
Timothy Merritt <timmybytes@gmail.com>
2020-12-01 11:01:40 -0800

    (feat): Add resume button, download

--- js/index.js
+++ js/index.js
@@ -35,11 +35,12 @@ footer.innerHTML = `&copy ${date} Timothy Merritt`;
 // TODO: REFACTOR
 const title = document.body.querySelector('.bio__title').style;
 const titleFade = document.body.querySelector('.bio__title-fade').style;
-const iconGit = document.body.querySelector('.bio__tagline-icons').childNodes[1].style;
-const iconDev = document.body.querySelector('.bio__tagline-icons').childNodes[3].style;
-const iconLinked = document.body.querySelector('.bio__tagline-icons').childNodes[5].style;
-const iconMusic = document.body.querySelector('.bio__tagline-icons').childNodes[7].style;
-const iconTwit = document.body.querySelector('.bio__tagline-icons').childNodes[9].style;
+const iconResume = document.body.querySelector('.bio__tagline-icons').childNodes[1].style;
+const iconGit = document.body.querySelector('.bio__tagline-icons').childNodes[3].style;
+const iconDev = document.body.querySelector('.bio__tagline-icons').childNodes[5].style;
+const iconLinked = document.body.querySelector('.bio__tagline-icons').childNodes[7].style;
+const iconMusic = document.body.querySelector('.bio__tagline-icons').childNodes[9].style;
+const iconTwit = document.body.querySelector('.bio__tagline-icons').childNodes[11].style;
 const linkDev = document.body.querySelector('.bio__tagline').childNodes[1].style;
 const linkWrite = document.body.querySelector('.bio__tagline').childNodes[3].style;
 const linkMusic = document.body.querySelector('.bio__tagline').childNodes[5].style;
@@ -59,6 +60,7 @@ const fade = () => {
   setTimeout(() => {
     title.display = 'none';
     titleFade.display = 'block';
+    iconResume.visibility = 'visible';
     iconGit.visibility = 'visible';
     iconDev.visibility = 'visible';
     iconLinked.visibility = 'visible';
